3b1bfad35a0ab0aa36ab7b3177833f6b855bedc1
Ahmad Fajruddin Syauqi <64742395+Syauqiaja@users.noreply.github.com>
2024-01-16 19:46:14 +0700

    feat: slicing favorite and search

---
+++ app/src/main/java/com/takehomechallange/syauqi/presentation/features/search/SearchFragment.kt
@@ -0,0 +1,9 @@
+package com.takehomechallange.syauqi.presentation.features.search
+
+import com.takehomechallange.syauqi.databinding.FragmentSearchBinding
+import com.takehomechallange.syauqi.presentation.base.BaseFragment
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(
+    FragmentSearchBinding::inflate
+) {
+}
